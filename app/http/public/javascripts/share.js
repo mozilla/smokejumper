a8e3bb337706866a34f836348d6ce3c39c5eac73
Simon Wex <simon@simonwex.com>
2012-08-31 13:07:17 -0700

    In an in-between state. Attempting to send the sender's offer over the socket.

--- app/http/public/javascripts/share.js
+++ app/http/public/javascripts/share.js
@@ -14,20 +14,11 @@ $(function(){
 
   window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-  var handlers = {
-    'share away': function(){
-      $('#shipper').show();
-    },
-    'incoming': function(){
-      $('#receiver').show();
-    }
-  }
-
   //TODO: Get this from config.
-  var socket = new WebSocket('ws://127.0.0.1:3000/' + document.location.pathname.substr(6));
+  var socket = new WebSocket('ws://127.0.0.1:3000' + document.location.pathname.substr(6));
 
   socket.onopen = function(){
-    console.log("open and ready for business");
+    console.log("Socket open.");
   };
 
   socket.onerror = function(error){
@@ -36,6 +27,7 @@ $(function(){
 
   socket.onmessage = function(rawMessage){
     try {
+      console.log(rawMessage.data);
       var message = JSON.parse(rawMessage.data);
       var handler = handlers[message.topic];
 
@@ -47,11 +39,72 @@ $(function(){
       }
     }
     catch (e) {
-      console.log('This doesn\'t look like a valid JSON: ', message.data);
+      console.log(e);
+      console.log({'This doesn\'t look like a valid JSON': message.data});
       return;
     }
   }
 
-  
+  socket.sendMessage = function(topic, data, callback){
+    console.log('Sending message: ' + topic);
+    if (typeof(data) == 'function')
+      callback = data;
+
+    var message = JSON.stringify({'topic': topic, 'data': data});
+
+    this.send(message, callback);
+  };
+
+  var peerConnection = new mozPeerConnection();
+
+  // The sender's path:
+  //    'share away' ... 'receiver connected' -> 'offerCreated' ...
+  // The receiver's path:
+  //    'incoming' ... 'share offer received'
+
+  var handlers = {
+    // Socket events for sharer:
+    'share away': function(){
+      $('#shipper').show();
+    },
+    'receiver connected': function(){
+      peerConnection.onRemoteStreamAdded = function(remoteStream){
+        console.log({"remote stream added": remoteStream});
+      }
+
+      // "reusing" the transport created for an audio channel because we don't have SDP set up 
+      // for data channels yet.
+      peerConnection.addStream(peerConnection.createFakeMediaStream("audio", true));
+
+      peerConnection.createOffer(handlers.offerCreated, handlers.offerCreationFailed);
+    },
+
+    // Socket events for receiving:
+    'incoming': function(){
+      $('#receiver').show();
+    },
+    'share offer received': function(offer){
+      console.log(offer);
+    },
+
+     // Local events
+    'offerCreated': function(offer){
+      console.log("offer created");
+
+      socket.sendMessage('shareOffer', offer);
+
+      peerConnection.setLocalDescription(offer, handlers.waitForOfferResponse, handlers.genericFailure);
+
+    },
+    'offerCreationFailed': function(s){
+      console.log({"offerCreationFailed": s});
+    },
+    'waitForOfferResponse': function(){
+      console.log('waiting for offer response');
+    },
+    'genericFailure': function(o){
+      console.log({'failure': o});
+    }
+  }
 });
 
